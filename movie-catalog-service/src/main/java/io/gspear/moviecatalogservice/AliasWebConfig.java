f0902c36429d4de59d4b780089729fd5bd2a6efa
pranaami <pranaami@synamedia.com>
2020-08-10 12:34:26 -0400

    docker-compose

---
+++ movie-catalog-service/src/main/java/io/gspear/moviecatalogservice/AliasWebConfig.java
@@ -0,0 +1,18 @@
+package io.gspear.moviecatalogservice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class AliasWebConfig implements WebMvcConfigurer {
+
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    //well, that's just for the compatibility with the toolbox...
+    //do not pay too much attention to it
+    registry.addViewController("/prometheus").setViewName("forward:/actuator/prometheus");
+  }
+}
